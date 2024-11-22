a94d1bedbff4ac8f551dcacec27553212b90d946
GabrielHEB <gabs.dev@hotmail.com>
2024-11-22 20:46:56 -0300

    Updates

---
+++ precache-manifest.de4bfe9b547aea2fdbfa249c6e895b0e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1bbfac28ea537e7c78d53eefbfac2cb",
+    "url": "/ReceitasApp/index.html"
+  },
+  {
+    "revision": "01dfd6c2175801b4839a",
+    "url": "/ReceitasApp/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "d3fc3df32a2dfe1053d2",
+    "url": "/ReceitasApp/static/css/main.09825c5d.chunk.css"
+  },
+  {
+    "revision": "01dfd6c2175801b4839a",
+    "url": "/ReceitasApp/static/js/2.32dd5b22.chunk.js"
+  },
+  {
+    "revision": "57fb86a905f06aed4b5091745659b335",
+    "url": "/ReceitasApp/static/js/2.32dd5b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3fc3df32a2dfe1053d2",
+    "url": "/ReceitasApp/static/js/main.026e69c1.chunk.js"
+  },
+  {
+    "revision": "1cd58e859f4c0ccaf99b",
+    "url": "/ReceitasApp/static/js/runtime-main.7fec3f8d.js"
+  },
+  {
+    "revision": "fd159fa36cc221651b655a085f12070c",
+    "url": "/ReceitasApp/static/media/background.fd159fa3.jpg"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/ReceitasApp/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/ReceitasApp/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/ReceitasApp/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "ecee03f3524e9d6a23ed5f3e4f186b32",
+    "url": "/ReceitasApp/static/media/logoreceitapp.ecee03f3.png"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/ReceitasApp/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/ReceitasApp/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/ReceitasApp/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/ReceitasApp/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/ReceitasApp/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
